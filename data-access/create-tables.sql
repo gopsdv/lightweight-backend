afe99e4f8f5ae0ba5527ebe1926205ef9229ea6b
gopsdv <gopi1031998@gmail.com>
2024-09-11 13:45:38 +0530

    ADD workouts and sets table

--- data-access/create-tables.sql
+++ data-access/create-tables.sql
@@ -1,5 +1,25 @@
-CREATE TABLE exercises (
+CREATE TABLE `exercises` (
   `id`       INT AUTO_INCREMENT NOT NULL,
-  `name`     VARCHAR(255) NOT NULL,
+  `name`     VARCHAR(255) NOT NULL UNIQUE,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE `workouts` (
+  `id` INT AUTO_INCREMENT NOT NULL,
+  `exercise_id` INT NOT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`exercise_id`) REFERENCES `exercises`(`id`)
+);
+
+CREATE TABLE `sets` (
+  `id`       INT AUTO_INCREMENT NOT NULL,
+  `workout_id` INT NOT NULL,
+  `set_num`  INT NOT NULL,
+  `reps` INT DEFAULT 0,
+  `preps` INT DEFAULT 0,
+  `rir` INT DEFAULT 0,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`workout_id`) REFERENCES `workouts`(`id`)
+);
+  
